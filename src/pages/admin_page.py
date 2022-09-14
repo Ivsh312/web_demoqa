3db0001d3e47a6e016d52b4fae549112ddd15fa1
Shyshkou_I <ishyshkou@ibagroup.eu>
2022-09-14 18:31:45 +0300

    Update structure for class AdminPage.

--- src/pages/admin_page.py
+++ src/pages/admin_page.py
@@ -7,26 +7,30 @@ from src.elements.elements import *
 from src.pages.componenrs.section import Section
 from src.pages.main_page import MainPage
 
-def a(c=12345):
-    ...
-
-class ab:
-    ...
 class AdminPage(MainPage):
 
     _currents_page_url = ADMIN_PAGE_URL
     current_status_indicator = Label(*status_indicator)
+    jms_name_l = Label(*jms_name)
+    status_indicator_l = Label(*status_indicator)
+    startup_btn = Button(*startup_btn)
+    status_label = Label(*status_label)
+
     jms_section_component = {
-        JMS_NAME: lambda: Label(*jms_name),
-        STATUS_INDICATOR: lambda: Label(*status_indicator),
-        STARTUP_BTN: lambda: Button(*startup_btn),
-        STATUS_LABEL: lambda: Label(*status_label)
+        JMS_NAME: jms_name_l,
+        STATUS_INDICATOR: status_indicator_l,
+        STARTUP_BTN: startup_btn,
+        STATUS_LABEL: status_label
     }
+    soap_radiobutton = RadioButton(*soap_radiobutton)
+    rest_json_radiobutton = RadioButton(*rest_json_radiobutton)
+    rest_xml_radiobutton = RadioButton(*rest_xml_radiobutton)
+    jdbc_radiobutton = RadioButton(*jdbc_radiobutton)
     data_access_mode = {
-        SOAP_RADIOBUTTON: lambda: RadioButton(*soap_radiobutton),
-        REST_JSON_RADIOBUTTON: lambda: RadioButton(*rest_json_radiobutton),
-        REST_XML_RADIOBUTTON: lambda: RadioButton(*rest_xml_radiobutton),
-        JDBC_RADIOBUTTON: lambda: RadioButton(*jdbc_radiobutton),
+        SOAP_RADIOBUTTON: soap_radiobutton,
+        REST_JSON_RADIOBUTTON: rest_json_radiobutton,
+        REST_XML_RADIOBUTTON: rest_xml_radiobutton,
+        JDBC_RADIOBUTTON: jdbc_radiobutton,
     }
 
 
@@ -44,25 +48,3 @@ class AdminPage(MainPage):
     def click_radiobutton(self, item: str) -> dict:
         self.data_access_mode_section[item].click()
         return {key: value.is_selected() for key, value in self.data_access_mode_section.items()}
-
-
-if __name__ == '__main__':
-
-
-    path_to_firefox = os.path.abspath('../../drivers/chromedriver.exe')
-    apage = AdminPage(webdriver=webdriver.Chrome(path_to_firefox))
-    apage.webdriver.get(url=ADMIN_PAGE_URL)
-
-    print(apage.current_status_indicator.text)
-    print(apage.change_jms_status())
-    apage.change_jms_status()
-    print(apage.current_status_indicator.text)
-    print(apage.click_radiobutton(REST_XML_RADIOBUTTON))
-    print(apage.current_status_indicator)
-
-    # mpage.login(username=test_login, password=test_password)
-    # mpage.button_menu.select_item(1)
-    # print(mpage.is_current_page())
-    # mpage.logout()
-    # mpage.webdriver.quit()
-    # print(mpage.is_logged_in)
