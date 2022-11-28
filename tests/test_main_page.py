43760e93e78c9f08d33a61be71f2434f86a4d635
Shyshkou_I <ishyshkou@ibagroup.eu>
2022-11-28 11:25:04 +0300

    folder for reports

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -43,5 +43,5 @@ class TestAdminPage:
     def test_navigation(self, main_page, expected_page):
         current_page, link_number = expected_page
         main_page.button_menu.select_item(expected_page[1])
-        time.sleep(3)
+        print(main_page.webdriver.current_url)
         assert current_page.is_current_page(), f'menu point {link_number}, dont lead to {type(current_page).__name__}'
