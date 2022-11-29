f5a867e3f86dea03892f1b7516a2b4ef9e838870
Shyshkou_I <ishyshkou@ibagroup.eu>
2022-11-29 13:58:22 +0300

    check for jenkins.

--- tests/test_admin_page.py
+++ tests/test_admin_page.py
@@ -1,7 +1,7 @@
 import pytest
 
 from exceptions.exeption import BasePageException
-from src.data.admin_page import REST_XML_RADIOBUTTON, ADMIN_PAGE_URL
+from src.data.admin_page import ADMIN_PAGE_URL
 from src.pages.admin_page import AdminPage
 
 
@@ -14,10 +14,8 @@ class TestAdminPage:
     def test_indicator(self, admin_page):
         status_before_click = admin_page.current_status_indicator.text
         admin_page.startup_btn.click()
-        print(status_before_click)
         try:
             admin_page.wait_until_element_param_dont_change(admin_page.status_label, 'text', status_before_click)
-            print(admin_page.current_status_indicator.text)
             assert True
         except BasePageException:
             assert False, "indicator dont changed status after click switcher"
